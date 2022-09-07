85838c1d92504b5b989091196a1aa2229ba28e00
Julian <williamsmjulian@gmail.com>
2022-09-07 10:34:00 -0400

    questions

---
+++ questions.js
@@ -0,0 +1,25 @@
+const timerEl = document.querySelector(".timerEL");
+
+
+
+var myQuestions = [
+	{
+		question: "What is Javascript",
+		answers: {
+			a: 'a language',
+			b: 'a house',
+			c: 'a car'
+		},
+		correctAnswer: 'a'
+	},
+	{
+		question: "Which of these is not javascript",
+		answers: {
+			a: 'a house',
+			b: 'a language',
+			c: 'a complex',
+		},
+		correctAnswer: 'c'
+	}
+];
+
